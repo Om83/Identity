5bccbe30995a190c3d24027e1cc713fcd1c25f80
omkrishna10 <omkrishna10@gmail.com>
2019-02-16 11:25:18 -0800

    Adding missing files

--- ActivityAndIntents/app/src/main/java/com/example/activityandintents/MainActivity.java
+++ ActivityAndIntents/app/src/main/java/com/example/activityandintents/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.activityandintents;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    static final int PICK_CONTACT_REQUEST = 1;  // The request code
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent myIntent = getIntent();
+
+        Button b = findViewById(R.id.SecondActivityBtn);
+        TextView tv = findViewById(R.id.MainActivityTextView);
+        String str1= myIntent.getStringExtra("FirstKey");
+        String str2= myIntent.getStringExtra("SecondKey");
+        tv.setText(str1+str2);
+
+        Button b1 = findViewById(R.id.MainActivityBtn);
+        String msg = "This message is from Main Activity";
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent navigateToNextPage = new Intent(v.getContext(),SecondActitity.class);
+                navigateToNextPage.putExtra("FirstKey","This message is from MainActivity");
+                navigateToNextPage.putExtra("SecondKey","This is additional message");
+                startActivity(navigateToNextPage);
+
+            }
+        });
     }
+    
+
+
 }
--- ActivityAndIntents/app/src/main/java/com/example/activityandintents/SecondActitity.java
+++ ActivityAndIntents/app/src/main/java/com/example/activityandintents/SecondActitity.java
@@ -1,11 +1,14 @@
 package com.example.activityandintents;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActitity extends AppCompatActivity {
 
@@ -13,17 +16,26 @@ public class SecondActitity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_actitity);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Intent myIntent = getIntent();
+
+        Button b = findViewById(R.id.SecondActivityBtn);
+        TextView tv = findViewById(R.id.textView);
+        String str1= myIntent.getStringExtra("FirstKey");
+        String str2= myIntent.getStringExtra("SecondKey");
+        tv.setText(str1+str2);
+        b.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent navigateToNextPage = new Intent(v.getContext(),MainActivity.class);
+                navigateToNextPage.putExtra("FirstKey","This message is from SecondActivity");
+                navigateToNextPage.putExtra("SecondKey","Hello Om");
+                startActivity(navigateToNextPage);
+
             }
         });
     }
 
 }
+
+
