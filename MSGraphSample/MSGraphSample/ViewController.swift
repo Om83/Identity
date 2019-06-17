7467b42c8f75d5151ee9d4a9b1144997803ad74a
Om83 <omkrishna10@gmail.com>
2019-06-16 17:29:10 -0700

    Graph Sample iOS

---
+++ MSGraphSample/MSGraphSample/ViewController.swift
@@ -0,0 +1,259 @@
+//
+//  ViewController.swift
+//  MSGraphSample
+//
+//  Created by Om Krishna on 6/16/19.
+//  Copyright © 2019 Microsoft. All rights reserved.
+//
+
+import UIKit
+import MSAL
+
+class ViewController: UIViewController {
+
+    let kClientID = "20f9a4fb-aef0-4b81-b3ad-850ba305e93a"
+    
+    // Additional variables for Auth and Graph API
+    let kGraphURI = "https://graph.microsoft.com/v1.0/me/"
+    let kScopes: [String] = ["https://graph.microsoft.com/user.read"]
+    let kAuthority = "https://login.microsoftonline.com/common"
+    var accessToken = String()
+    var applicationContext : MSALPublicClientApplication?
+    
+    var loggingText: UITextView!
+    var signOutButton: UIButton!
+    var callGraphButton: UIButton!
+    
+    func initUI() {
+        // Add call Graph button
+        callGraphButton  = UIButton()
+        callGraphButton.translatesAutoresizingMaskIntoConstraints = false
+        callGraphButton.setTitle("Call Microsoft Graph API", for: .normal)
+        callGraphButton.setTitleColor(.blue, for: .normal)
+        callGraphButton.addTarget(self, action: #selector(callGraphAPI(_:)), for: .touchUpInside)
+        self.view.addSubview(callGraphButton)
+        
+        callGraphButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        callGraphButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 50.0).isActive = true
+        callGraphButton.widthAnchor.constraint(equalToConstant: 300.0).isActive = true
+        callGraphButton.heightAnchor.constraint(equalToConstant: 50.0).isActive = true
+        
+        // Add sign out button
+        signOutButton = UIButton()
+        signOutButton.translatesAutoresizingMaskIntoConstraints = false
+        signOutButton.setTitle("Sign Out", for: .normal)
+        signOutButton.setTitleColor(.blue, for: .normal)
+        signOutButton.setTitleColor(.gray, for: .disabled)
+        signOutButton.addTarget(self, action: #selector(signOut(_:)), for: .touchUpInside)
+        self.view.addSubview(signOutButton)
+        
+        signOutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        signOutButton.topAnchor.constraint(equalTo: callGraphButton.bottomAnchor, constant: 10.0).isActive = true
+        signOutButton.widthAnchor.constraint(equalToConstant: 150.0).isActive = true
+        signOutButton.heightAnchor.constraint(equalToConstant: 50.0).isActive = true
+        signOutButton.isEnabled = false
+        
+        // Add logging textfield
+        loggingText = UITextView()
+        loggingText.isUserInteractionEnabled = false
+        loggingText.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(loggingText)
+        
+        loggingText.topAnchor.constraint(equalTo: signOutButton.bottomAnchor, constant: 10.0).isActive = true
+        loggingText.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 10.0).isActive = true
+        loggingText.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 10.0).isActive = true
+        loggingText.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 10.0).isActive = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initUI()
+        do {
+            try self.initMSAL()
+        } catch let error {
+            self.loggingText.text = "Unable to create Application Context \(error)"
+        }
+    }
+    
+    func initMSAL() throws {
+        
+        guard let authorityURL = URL(string: kAuthority) else {
+            self.loggingText.text = "Unable to create authority URL"
+            return
+        }
+        
+        let authority = try MSALAADAuthority(url: authorityURL)
+        
+        let msalConfiguration = MSALPublicClientApplicationConfig(clientId: kClientID, redirectUri: nil, authority: authority)
+        self.applicationContext = try MSALPublicClientApplication(configuration: msalConfiguration)
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        guard let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String else {
+            return false
+        }
+        
+        return MSALPublicClientApplication.handleMSALResponse(url, sourceApplication: sourceApplication)
+    }
+    
+    @objc func callGraphAPI(_ sender: UIButton) {
+        
+        guard let currentAccount = self.currentAccount() else {
+            // We check to see if we have a current logged in account.
+            // If we don't, then we need to sign someone in.
+            acquireTokenInteractively()
+            return
+        }
+        
+        acquireTokenSilently(currentAccount)
+    }
+    
+    func currentAccount() -> MSALAccount? {
+        
+        guard let applicationContext = self.applicationContext else { return nil }
+        
+        // We retrieve our current account by getting the first account from cache
+        // In multi-account applications, account should be retrieved by home account identifier or username instead
+        
+        do {
+            let cachedAccounts = try applicationContext.allAccounts()
+            if !cachedAccounts.isEmpty {
+                return cachedAccounts.first
+            }
+        } catch let error as NSError {
+            self.updateLogging(text: "Didn't find any accounts in cache: \(error)")
+        }
+        
+        return nil
+    }
+    
+    func acquireTokenInteractively() {
+        
+        guard let applicationContext = self.applicationContext else { return }
+        // #1
+        let parameters = MSALInteractiveTokenParameters(scopes: kScopes)
+        // #2
+        applicationContext.acquireToken(with: parameters) { (result, error) in
+            // #3
+            if let error = error {
+                self.updateLogging(text: "Could not acquire token: \(error)")
+                return
+            }
+            guard let result = result else {
+                self.updateLogging(text: "Could not acquire token: No result returned")
+                return
+            }
+            // #4
+            self.accessToken = result.accessToken
+            self.updateLogging(text: "Access token is \(self.accessToken)")
+            self.updateSignOutButton(enabled: true)
+            self.getContentWithToken()
+        }
+    }
+    
+    func acquireTokenSilently(_ account : MSALAccount!) {
+        guard let applicationContext = self.applicationContext else { return }
+        let parameters = MSALSilentTokenParameters(scopes: kScopes, account: account)
+        
+        applicationContext.acquireTokenSilent(with: parameters) { (result, error) in
+            if let error = error {
+                let nsError = error as NSError
+                if (nsError.domain == MSALErrorDomain) {
+                    if (nsError.code == MSALError.interactionRequired.rawValue) {
+                        DispatchQueue.main.async {
+                            self.acquireTokenInteractively()
+                        }
+                        return
+                    }
+                }
+                self.updateLogging(text: "Could not acquire token silently: \(error)")
+                return
+            }
+            
+            guard let result = result else {
+                self.updateLogging(text: "Could not acquire token: No result returned")
+                return
+            }
+            
+            self.accessToken = result.accessToken
+            self.updateLogging(text: "Refreshed Access token is \(self.accessToken)")
+            self.updateSignOutButton(enabled: true)
+            self.getContentWithToken()
+        }
+    }
+    
+    func getContentWithToken() {
+        // Specify the Graph API endpoint
+        let url = URL(string: kGraphURI)
+        var request = URLRequest(url: url!)
+        
+        // Set the Authorization header for the request. We use Bearer tokens, so we specify Bearer + the token we got from the result
+        request.setValue("Bearer \(self.accessToken)", forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            if let error = error {
+                self.updateLogging(text: "Couldn't get graph result: \(error)")
+                return
+            }
+            
+            guard let result = try? JSONSerialization.jsonObject(with: data!, options: []) else {
+                
+                self.updateLogging(text: "Couldn't deserialize result JSON")
+                return
+            }
+            
+            self.updateLogging(text: "Result from Graph: \(result))")
+            
+            }.resume()
+    }
+    
+    @objc func signOut(_ sender: UIButton) {
+        
+        guard let applicationContext = self.applicationContext else { return }
+        
+        guard let account = self.currentAccount() else { return }
+        
+        do {
+            
+            /**
+             Removes all tokens from the cache for this application for the provided account
+             
+             - account:    The account to remove from the cache
+             */
+            
+            try applicationContext.remove(account)
+            self.loggingText.text = ""
+            self.signOutButton.isEnabled = false
+            
+        } catch let error as NSError {
+            
+            self.updateLogging(text: "Received error signing account out: \(error)")
+        }
+    }
+    
+    func updateLogging(text : String) {
+        
+        if Thread.isMainThread {
+            self.loggingText.text = text
+        } else {
+            DispatchQueue.main.async {
+                self.loggingText.text = text
+            }
+        }
+    }
+    
+    func updateSignOutButton(enabled : Bool) {
+        if Thread.isMainThread {
+            self.signOutButton.isEnabled = enabled
+        } else {
+            DispatchQueue.main.async {
+                self.signOutButton.isEnabled = enabled
+            }
+        }
+    }
+
+}
+
